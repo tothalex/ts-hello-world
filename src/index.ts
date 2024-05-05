47e888f503308c257244950662aeee6dfd6a67e4
Toth Alex <tothalex@Toths-MacBook-Pro.local>
2024-05-05 19:24:28 +0200

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello, World!')
+import Fastify from 'fastify'
+import FastifyStatic from '@fastify/static'
+import path from 'node:path'
+
+const fastify = Fastify({
+  logger: true,
+})
+
+fastify.register(FastifyStatic, {
+  root: path.join(__dirname, 'public'),
+})
+
+fastify.listen({ port: 3000 }, function (err, _) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
